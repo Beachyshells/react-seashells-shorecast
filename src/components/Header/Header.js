3bce95d6c299c709729d2a62e42fea867649b4e6
Beachyshells <151598119+Beachyshells@users.noreply.github.com>
2025-10-08 18:38:59 -0400

    affixed the app header to the top of the page and gave it a nice opacity to look like frosted glass

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import "./Header.css";
 export default function Header() {
   return (
-    <div className="Header">
+    <div className="Header app-header">
       <h1 className="title">SeaShell's Shorecast</h1>
       <p className="sub-title">Local Lakeside Weather- it's a shorething</p>
     </div>
