9fdfa5fbdd7e27445f5b1e94a5c7bfdf854d9ece
Beachyshells <151598119+Beachyshells@users.noreply.github.com>
2025-10-09 11:00:02 -0400

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,10 +3,12 @@ export default function Header(props) {
   return (
     <div className="Header app-header">
       <h1 className="title header-title">SeaShell's Shorecast</h1>
-      <p className="sub-title">Local Lakeside Weather- it's a shorething</p>
-      <button onClick={props.toggleTheme} className="theme-toggle">
-        {props.theme === "light" ? "🌙" : "☀️"}
-      </button>
+      <div className="d-flex justify-content-center">
+        <p className="sub-title">Local Lakeside Weather- it's a shorething</p>
+        <button onClick={props.toggleTheme} className="theme-toggle">
+          {props.theme === "light" ? "🌙" : "☀️"}
+        </button>
+      </div>
     </div>
   );
 }
