6bb2c83b6fbefe719e6f4d11304e137a879a9db4
Beachyshells <151598119+Beachyshells@users.noreply.github.com>
2025-10-09 15:10:41 -0400

    gave the dark mode refining colors. added the new footer background. gave the body the classlist logic so i could use it everywhere

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Body from "./components/Body/Body";
@@ -9,8 +9,12 @@ function App() {
   const toggleTheme = () => {
     setTheme(theme === "light" ? "dark" : "light");
   };
+  useEffect(() => {
+    document.body.classList.remove("light", "dark");
+    document.body.classList.add(theme);
+  }, [theme]);
   return (
-    <div className={`container App  ${theme}`}>
+    <div className="container App">
       <Header toggleTheme={toggleTheme} theme={theme} />
       <Body />
       <Footer />
